20b82fa63c4109f574ec7469f1a22c3204295b47
francescapunzetti <francesca.punzet@gmail.com>
2023-03-16 19:04:14 +0100

    add edges analysis

--- net.m
+++ net.m
@@ -38,92 +38,64 @@ A13 = corrcoef(T13);
 A14 = corrcoef(T14);
 A15 = corrcoef(T15);
 
-% tl = tiledlayout("flow",'TileSpacing','Compact');
-% nexttile(tl)
-% h = imagesc(A6);
-% clim([-1 1]);
-% title('2501 - 3000')
-% nexttile(tl);
-% h = imagesc(A7);
-% clim([-1 1]);
-% title('3001 - 3500')
-% nexttile(tl);
-% h = imagesc(A8);
-% clim([-1 1]);
-% title('3501 - 4000')
-% nexttile(tl);
-% h = imagesc(A9);
-% clim([-1 1]);
-% title('4001 - 4500')
-% boh = nexttile(tl);
-% h = imagesc(A10);
-% clim([-1 1]);
-% title('4501 - 5000')
-% nexttile(tl);
-% heatmap(A6);
-% clim([-1 1]);
-% nexttile(tl);
-% heatmap(A7);
-% clim([-1 1]);
-% nexttile(tl);
-% heatmap(A8);
-% clim([-1 1]);
-% nexttile(tl);
-% heatmap(A9);
-% clim([-1 1]);
-% nexttile(tl);
-% heatmap(A10);
-% clim([-1 1]);
-% nexttile(tl);
-% heatmap(A11);
-% clim([-1 1]);
-% nexttile(tl);
-% heatmap(A12);
-% clim([-1 1]);
-% nexttile(tl);
-% heatmap(A13);
-% clim([-1 1]);
-% nexttile(tl);
-% heatmap(A14);
-% clim([-1 1]);
-% nexttile(tl);
-% heatmap(A15);
-% clim([-1 1]);
-% nexttile(tl);
-% h = imagesc(A);
-% clim([-1 1]);
-% title('Final adj matrx')
-% 
-% J = customcolormap([0 0.5 1], {'#0025B3','#ffffff','#FF0008'});
-% colormap(J);
-% cb = colorbar;
-% cb.Layout.Tile = 'east';
+figure; 
+tl = tiledlayout("flow",'TileSpacing','Compact');
+nexttile(tl)
+h = imagesc(A6);
+clim([-1 1]);
+title('2501 - 3000')
+nexttile(tl);
+h = imagesc(A7);
+clim([-1 1]);
+title('3001 - 3500')
+nexttile(tl);
+h = imagesc(A8);
+clim([-1 1]);
+title('3501 - 4000')
+nexttile(tl);
+h = imagesc(A9);
+clim([-1 1]);
+title('4001 - 4500')
+boh = nexttile(tl);
+h = imagesc(A10);
+clim([-1 1]);
+title('4501 - 5000')
+h = imagesc(A);
+clim([-1 1]);
+title('Final adj matrx')
+
+J = customcolormap([0 0.5 1], {'#0025B3','#ffffff','#FF0008'});
+colormap(J);
+cb = colorbar;
+cb.Layout.Tile = 'east';
+
 matrices = {A, A1, A2, A3, A4, A5 ,A6, A7, A8, A9, A10, A11, A12, A13, A14, A15};
 a = matrices;
+
+figure;
+for k = 1:16
+subplot(4,4, k)
+histogram(a{1,k},64, 'Normalization', 'probability');
+ylim([0, 0.1]);
+xlim([-1, 1]);
+xlabel('Coefficient');
+ylabel('Probability');
+if k == 1
+    title('Adiacency matrix');
+end
+if k ~= 1
+    str = sprintf('A %d', k-1);
+    title(str);
+end
+end
+
 % 
-% for k = 1:16
-% subplot(4,4, k)
-% histogram(a{1,k},64, 'Normalization', 'probability');
-% ylim([0, 0.1]);
-% xlim([-1, 1]);
-% xlabel('Coefficient');
-% ylabel('Probability');
-% if k == 1
-%     title('Adiacency matrix');
-% end
-% if k ~= 1
-%     str = sprintf('A %d', k-1);
-%     title(str);
-% end
-% % end
-
-
 % Draw network
 counts = 1;
 set =  0:0.05:1;
 for j =  0:0.05:1
 for k = 1:16
-%subplot(4,4, k)
+subplot(4,4, k)
 Th = j;
 A_th = a{1,k};
 comparator = a{1,k}==1; 
@@ -146,7 +118,26 @@ end
 counts = counts +1; 
 end
 
+figure;
 boxplot(N_edges, "Labels",set)
 xlabel('Correlation coefficient threshold')
-% bar(set, N_edges, 'DisplayName','N_edges')
 
+figure;
+bar(set, N_edges, 'DisplayName','N_edges')
+hold on
+yline(280)
+
+A_th = a{1,1};
+comparator = a{1,1}==1; 
+A_th(comparator) = 0;
+comparator = a{1,1}<0.2;
+A_th(comparator) = 0;
+G = graph(A_th);
+
+
+figure;
+p = plot(G,'Layout','force','EdgeAlpha',0.5,'NodeColor','r');
+deg_ranks = centrality(G,'degree','Importance',G.Edges.Weight);
+edges = linspace(min(deg_ranks),max(deg_ranks),7);
+bins = discretize(deg_ranks,edges);
+p.MarkerSize = bins;
